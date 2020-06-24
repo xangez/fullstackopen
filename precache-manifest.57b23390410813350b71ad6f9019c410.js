4d7ab0cafd8e787ee977253051ddd0f1da94d8d4
xangez <xangez348@gmail.com>
2020-06-24 18:06:36 +1000

    Updates

---
+++ precache-manifest.57b23390410813350b71ad6f9019c410.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d5543321c8ce7cf0065c64fecbd7ed7",
+    "url": "/react-practise/index.html"
+  },
+  {
+    "revision": "d96d459b0c5766c5620e",
+    "url": "/react-practise/static/js/2.5938bfca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-practise/static/js/2.5938bfca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfa75c3935ed047fa2e7",
+    "url": "/react-practise/static/js/main.3617e332.chunk.js"
+  },
+  {
+    "revision": "dc3f4ef4e0f444b7113b",
+    "url": "/react-practise/static/js/runtime-main.4be084fa.js"
+  }
+]);
