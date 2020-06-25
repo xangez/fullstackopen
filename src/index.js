b18d3a7a0dae89dfc4f2c7c91f6a026f3f5f8187
xangez <xangez348@gmail.com>
2020-06-25 22:34:57 +1000

    unicafe done

--- src/index.js
+++ src/index.js
@@ -1,21 +1,38 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
 
-const Statistics = ({ good, neutral, bad }) => {
+const Button = ({handleClick, text}) => {
+	return <button onClick={handleClick}>{text}</button>;
+};
+
+const Statistic = ({text, value}) => {
+	return (
+		<tr>
+			<td>{text}</td>
+			<td>{value}</td>
+		</tr>
+	);
+};
+
+const Statistics = ({good, neutral, bad}) => {
 	let all = good + bad + neutral;
 	let average = (good + bad * -1) / all;
 	let positive = good / all * 100;
 
+	if (all === 0) {
+		return <p>No feedback given</p>;
+	}
 	return (
-		<div>
-			<h1>Statistics</h1>
-			<p>good {good}</p>
-			<p>neutral {neutral}</p>
-			<p>bad {bad}</p>
-			<p>all {all}</p>
-			<p>average {average}</p>
-			<p>positive {positive}%</p>
-		</div>
+    <table>
+      <tbody>
+        <Statistic text="good" value={good} />
+        <Statistic text="neutral" value={neutral} />
+        <Statistic text="bad" value={bad} />
+        <Statistic text="all" value={all} />
+        <Statistic text="average" value={average} />
+        <Statistic text="postive" value={positive} />
+      </tbody>
+    </table>
 	);
 };
 
@@ -23,14 +40,16 @@ const App = () => {
 	const [good, setGood] = useState(0);
 	const [neutral, setNeutral] = useState(0);
 	const [bad, setBad] = useState(0);
+
 	return (
-		<div>
+		<>
 			<h1>Give Feedback</h1>
-			<button onClick={() => setGood(good + 1)}>good</button>
-			<button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-			<button onClick={() => setBad(bad + 1)}>bad</button>
-			<Statistics good={good} bad={bad} neutral={neutral} />
-		</div>
+			<Button handleClick={() => setGood(good + 1)} text="Good" />
+			<Button handleClick={() => setNeutral(neutral + 1)} text="Neutral" />
+			<Button handleClick={() => setBad(bad + 1)} text="Bad" />
+			<h1>Statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+		</>
 	);
 };
 
