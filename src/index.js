54834a1344f3816dfa40c539207ae81dcf002815
xangez <xangez348@gmail.com>
2020-06-29 01:52:26 +1000

    starting part 2c

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
+import axios from "axios";
+
+axios.get("http://localhost:3001/notes").then((response) => {
+  const notes = response.data;
+  console.log(notes);
+});
 
 ReactDOM.render(<App />, document.getElementById("root"));
