12b52aa9e15826971538e762a092f71ea4b8c32b
xangez <xangez348@gmail.com>
2020-06-29 23:35:36 +1000

    done weather

---
+++ src/components/cityWeather.js
@@ -0,0 +1,33 @@
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+
+const CityWeather = ({name, city}) => {
+  const [weather, setWeather] = useState("");
+
+  useEffect(() => {
+    const key = `${process.env.REACT_APP_WEATHER_KEY}`;
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city},${name}&units=metric&APPID=${key}`;
+    axios.get(url).then((response) => {
+      console.log(response.data);
+      setWeather(response.data);
+    });
+  }, []);
+
+  if (weather === "") {
+    return <div></div>;
+  }
+
+  return (
+    <>
+      <h3>Weather in {city}</h3>
+      <p>Temperature: {weather.main.temp} Celcius</p>
+      <img
+        src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+        alt="weather-icon"
+      ></img>
+      <p>Wind: {weather.wind.speed} m/s</p>
+    </>
+  );
+};
+
+export default CityWeather;
--- src/components/countryDetails.js
+++ src/components/countryDetails.js
@@ -1,18 +1,22 @@
 import React from "react";
+import CityWeather from "./cityWeather";
 
 const CountryDetails = ({country}) => {
+  const {name, capital, population, languages, flag} = country;
+
   return (
     <>
-      <h2>{country.name}</h2>
-      <p>{country.capital}</p>
-      <p>Population: {country.population}</p>
-      <h3>Languages</h3>
+      <h2>{name}</h2>
+      <p>{capital}</p>
+      <p>Population: {population}</p>
+      <h3>Spoken languages</h3>
       <ul>
-        {country.languages.map((lang, index) => (
+        {languages.map((lang, index) => (
           <li key={index}>{lang.name}</li>
         ))}
       </ul>
-      <img style={{width: "100px"}} src={country.flag} alt="flag"></img>
+      <img style={{width: "100px"}} src={flag} alt="flag"></img>
+      <CityWeather city={capital} name={name} />
     </>
   );
 };
