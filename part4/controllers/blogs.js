d0478d0c5ffde8f420452860aed7a4120b2c93cb
xangez <xangez348@gmail.com>
2020-08-06 22:38:44 +1000

    setup folders and files of blog app

---
+++ part4/controllers/blogs.js
@@ -0,0 +1,59 @@
+const blogsRouter = require("express").Router();
+const Blog = require("../models/blog");
+
+blogsRouter.get("/", (request, response) => {
+  Blog.find({}).then((blog) => {
+    response.json(blog);
+  });
+});
+
+blogsRouter.get("/:id", (request, response, next) => {
+  Blog.findById(request.params.id)
+    .then((blog) => {
+      if (blog) {
+        response.json(blog);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
+
+blogsRouter.post("/", (request, response, next) => {
+  const body = request.body;
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: 0,
+  });
+  blog
+    .save()
+    .then((savedBlog) => {
+      response.json(savedBlog);
+    })
+    .catch((error) => error);
+});
+
+blogsRouter.delete("/:id", (request, response, next) => {
+  Blog.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+blogsRouter.put("/:id", (request, response, next) => {
+  const body = request.body;
+  const blog = {
+    title: body.title,
+    author: body.author,
+  };
+  Blog.findByIdAndUpdate(request.params.id, blog, {new: true})
+    .then((updatedBlog) => {
+      response.json(updatedBlog);
+    })
+    .catch((error) => next(error));
+});
+
+module.exports = blogsRouter;
