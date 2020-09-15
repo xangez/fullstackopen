10bdab0d750bf048b28d05080abcbb7ddae14502
xangez <xangez348@gmail.com>
2020-09-16 00:19:44 +1000

    remove 'required' in schema

--- part4/models/blog.js
+++ part4/models/blog.js
@@ -4,19 +4,15 @@ const mongoose = require("mongoose");
 const blogSchema = mongoose.Schema({
   title: {
     type: String,
-    required: true,
   },
   author: {
     type: String,
-    required: true,
   },
   url: {
     type: String,
-    required: true,
   },
   likes: {
     type: Number,
-    required: true,
   },
 });
 
