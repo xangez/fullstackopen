ee88f01f0d0f454392a117d4a516278ad0848c26
xangez <xangez348@gmail.com>
2020-09-19 15:21:52 +1000

    added authorization

--- part5/src/services/blogs.js
+++ part5/src/services/blogs.js
@@ -21,7 +21,10 @@ const create = async (newObject) => {
 };
 
 const update = async (id, newObject) => {
-  const response = await axios.put(`${baseUrl}/${id}`, newObject);
+  const config = {
+    headers: {Authorization: token},
+  };
+  const response = await axios.put(`${baseUrl}/${id}`, newObject, config);
   return await response.data;
 };
 
