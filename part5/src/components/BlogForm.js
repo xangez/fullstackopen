754bcff8a110e483bb2294a21a96afeb2e967c93
xangez <xangez348@gmail.com>
2020-09-19 15:21:13 +1000

    separate blogform component

---
+++ part5/src/components/BlogForm.js
@@ -0,0 +1,44 @@
+import React, {useState} from "react";
+
+const BlogForm = ({createBlog}) => {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [url, setUrl] = useState("");
+
+  const addBlog = (event) => {
+    event.preventDefault();
+    createBlog({
+      title: title,
+      author: author,
+      url: url,
+    });
+    setTitle("");
+    setAuthor("");
+    setUrl("");
+  };
+
+  return (
+    <div>
+      <div>
+        <h2>Create new blog</h2>
+        <form onSubmit={addBlog}>
+          <div>
+            Title:
+            <input value={title} onChange={({target}) => setTitle(target.value)} />
+          </div>
+          <div>
+            Author:
+            <input value={author} onChange={({target}) => setAuthor(target.value)} />
+          </div>
+          <div>
+            Url:
+            <input value={url} onChange={({target}) => setUrl(target.value)} />
+          </div>
+          <button type="submit">save</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default BlogForm;
